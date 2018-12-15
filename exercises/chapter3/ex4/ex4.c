ef081d2dcb9cf244899b1818277f4c143fa1e29a
Travis <travis.schnider@gmail.com>
2018-12-15 00:20:53 -0700

    reorganize directories. complete coding exercises only for ch3.

---
+++ exercises/chapter3/ex4/ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int val1, val2, result;
+
+  val1 = 15;
+  val2 = 87;
+  result = val1 - val2;
+
+  printf("%i - %i = %i\n", val1, val2, result);
+
+  return(0);
+}
---
+++ exercises/chapter3/ex5/ex5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int sum;
+
+  // Compute Result
+  sum = 25 + 37 - 19;
+
+  // Display Result
+  printf("The answer is %i.\n", sum);
+
+  return 0;
+}
