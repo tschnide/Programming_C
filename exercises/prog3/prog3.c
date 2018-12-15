50e543e659d0c99f49038620bf36424535440a71
Travis <travis.schnider@gmail.com>
2018-12-14 23:57:40 -0700

    add program 3

---
+++ exercises/prog3/prog3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+  int val1, val2, sum;  /* Single line declaration */ 
+  
+  val1 = 50;
+  val2 = 25;
+  sum = val1 + val2;
+
+  printf("The sum of %i, and %i is %i.\n", val1, val2, sum);
+
+  return 0;
+}
+
